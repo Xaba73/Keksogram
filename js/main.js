18d835547c006dc08487354445648ad3b1bea995
Xaba73 <wowforxab@mail.ru>
2022-09-13 07:36:40 +0300

    Перенос на другое рабочее пространство

--- js/main.js
+++ js/main.js
@@ -1,14 +1,60 @@
 'use strict';
-function randomNumber(min, max){
-  if (min < 0 || max < 0){
+
+const PHOTO_COUNTER = 25;
+const NAMES = [
+  'Александр ',
+  'Михаил',
+  'Максим',
+  'Лев',
+  'Артем',
+  'Мария',
+  'Дарья',
+  'Татьяна',
+  'Варвара',
+  'Алефтина',
+]
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+]
+function randomNumber(min, max) {
+  if (min < 0 || max < 0) {
     return -1;
   }
 
-  if ( min > max){
+  if (min > max) {
     [min, max] = [max, min]
   }
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
+
 const stringLengthCheck = (string, counter) => string.length <= counter;
 
-console.log(stringLengthCheck('Как насчёт мягких французких булок?', 140))
+let photos = new Array(PHOTO_COUNTER).fill(null).map(addPhotoDescription);
+
+function makeComment() {
+
+}
+
+function addPhotoDescription() {
+
+  let photoDescription = {
+    id: '',
+    url: '',
+    description: '',
+    likes: '',
+    comments: '',
+
+  }
+  return photoDescription;
+}
+
+
+
+console.log(photos);
